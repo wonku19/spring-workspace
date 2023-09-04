66e898677072bd9740fa537af04e60f0fbceb392
kwy8686 <133620204+wonku19@users.noreply.github.com>
2023-09-04 17:37:51 +0900

    0904-2

--- 05_MVC_Board/src/main/webapp/WEB-INF/views/board/view.jsp
+++ 05_MVC_Board/src/main/webapp/WEB-INF/views/board/view.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 pageEncoding="UTF-8"%>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib uri="http://www.springframework.org/security/tags" prefix="sec" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html data-bs-theme="dark">
   <head>
@@ -47,8 +49,12 @@ pageEncoding="UTF-8"%>
           <label for="writer">Writer</label>
           <input type="text" readonly value="${vo.writer}" id="writer" name="writer" class="form-control" />
         </div>
-        <a class="btn btn-outline-warning" href="/board/update?no=${vo.no}">수정</a>
-        <a class="btn btn-outline-danger" href="/board/delete?no=${vo.no}">삭제</a>
+        
+        <sec:authentication property="principal" var="info"/>
+        <c:if test="${vo.writer eq info.username}">
+        	<a class="btn btn-outline-warning" href="/board/update?no=${vo.no}">수정</a>
+        	<a class="btn btn-outline-danger" href="/board/delete?no=${vo.no}">삭제</a>
+        </c:if>
       </form>
     </div>
   </body>
---
+++ 06_SpringSecurity/06_SpringSecurity/src/main/java/com/kh/security/LoginSuccessHandler.java
@@ -0,0 +1,41 @@
+package com.kh.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		
+		List<String> roleList = new ArrayList();
+		
+		authentication.getAuthorities().forEach(auth -> {
+			System.out.println("auth : " + auth);
+			roleList.add(auth.getAuthority());
+		});
+		
+		if(roleList.contains("ROLE_MEMBER")) {
+			response.sendRedirect("/member");
+			return;
+		}
+		
+		if(roleList.contains("ROLE_ADMIN")) {
+			response.sendRedirect("/admin");
+			return;
+		}
+		
+		response.sendRedirect("/all");
+		
+	}
+
+}
